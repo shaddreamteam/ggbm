9e53aecb07ed401807fe11fef7a51bff5a32d6f7
Ivan Baskov <dest.31337@gmail.com>
2018-05-03 22:39:35 +0300

    first parallel

---
+++ cpp/src/TaskQueue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ivan Baskov on 03/05/2018.
+//
+
+#include "TaskQueue.h"
---
+++ cpp/src/TaskQueue.h
@@ -0,0 +1,14 @@
+//
+// Created by Ivan Baskov on 03/05/2018.
+//
+
+#ifndef CPP_TASKQUEUE_H
+#define CPP_TASKQUEUE_H
+
+
+class TaskQueue {
+
+};
+
+
+#endif //CPP_TASKQUEUE_H
